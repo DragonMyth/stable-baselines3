c0388e3e21c2d9161bc893ac8b4d2e9f4b5b43b0
yunbo <yunbo@yunbo-Ubuntu-18.04>
2021-04-28 15:25:07 -0400

    PPOWithDynamicKL

--- stable_baselines3/__init__.py
+++ stable_baselines3/__init__.py
@@ -7,7 +7,7 @@ from stable_baselines3.her import HER
 from stable_baselines3.ppo import PPO
 from stable_baselines3.sac import SAC
 from stable_baselines3.td3 import TD3
-
+from stable_baselines3.ppo import PPOAdjustKL
 # Read version from file
 version_file = os.path.join(os.path.dirname(__file__), "version.txt")
 with open(version_file, "r") as file_handler:
--- stable_baselines3/ppo/__init__.py
+++ stable_baselines3/ppo/__init__.py
@@ -1,2 +1,3 @@
 from stable_baselines3.ppo.policies import CnnPolicy, MlpPolicy
 from stable_baselines3.ppo.ppo import PPO
+from stable_baselines3.ppo.ppo_adjust_kl import PPOAdjustKL
